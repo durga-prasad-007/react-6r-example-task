79dc33127b8db93ee8fb4d0605c332249b14a6d1
unknown <dporange.siripuram007@gmail.com>
2024-08-03 01:07:05 +0530

    this is the first commit messege

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>REACT JS</h1>
+    <p>react is an open-source javascript libraray for building user interface</p>
+
+    </>
   );
 }
 
